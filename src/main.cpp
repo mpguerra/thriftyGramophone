5903abbcb2bdd652c2fcdfa96b673d7a3fb51339
Maria Pilar Guerra Arias <pili@piliguerra.com>
2013-02-09 14:01:53 +0100

    fixed indentation

--- src/main.cpp
+++ src/main.cpp
@@ -8,34 +8,34 @@ using namespace ThriftyGramophone;
 using namespace std;
 
 static gboolean bus_call (
-        GstBus     *bus,
-        GstMessage *msg,
-        gpointer    data)
+    GstBus     *bus,
+    GstMessage *msg,
+    gpointer    data)
 {
   GMainLoop *loop = (GMainLoop *) data;
 
   switch (GST_MESSAGE_TYPE (msg)) {
 
-    case GST_MESSAGE_EOS:
-      g_print ("End of stream\n");
-      g_main_loop_quit (loop);
-      break;
+  case GST_MESSAGE_EOS:
+    g_print ("End of stream\n");
+    g_main_loop_quit (loop);
+    break;
 
-    case GST_MESSAGE_ERROR: {
-      gchar  *debug;
-      GError *error;
+  case GST_MESSAGE_ERROR: {
+    gchar  *debug;
+    GError *error;
 
-      gst_message_parse_error (msg, &error, &debug);
-      g_free (debug);
+    gst_message_parse_error (msg, &error, &debug);
+    g_free (debug);
 
-      g_printerr ("Error: %s\n", error->message);
-      g_error_free (error);
+    g_printerr ("Error: %s\n", error->message);
+    g_error_free (error);
 
-      g_main_loop_quit (loop);
-      break;
-    }
-    default:
-      break;
+    g_main_loop_quit (loop);
+    break;
+  }
+  default:
+    break;
   }
 
   return TRUE;
@@ -55,72 +55,71 @@ int main(int argc, char **argv)
   // Check input arguments
   if(argc!=2)
   {
-      g_printerr ("Usage: %s <MP3 filename>\n", argv[0]);
-      return -1;
+    g_printerr ("Usage: %s <MP3 filename>\n", argv[0]);
+    return -1;
   }
 
   /* Create gstreamer elements */
-   pipeline = gst_pipeline_new ("audio-player");
-   source   = gst_element_factory_make ("filesrc","file-source");
-   decoder  = gst_element_factory_make ("mad","mad");
-   sink     = gst_element_factory_make ("autoaudiosink", "audio-output");
+  pipeline = gst_pipeline_new ("audio-player");
+  source   = gst_element_factory_make ("filesrc","file-source");
+  decoder  = gst_element_factory_make ("mad","mad");
+  sink     = gst_element_factory_make ("autoaudiosink", "audio-output");
 
-   if (!pipeline || !source || !decoder || !sink) {
-      g_printerr ("One element could not be created. Exiting.\n");
-      return -1;
-    }
+  if (!pipeline || !source || !decoder || !sink) {
+    g_printerr ("One element could not be created. Exiting.\n");
+    return -1;
+  }
 
-    /* Set up the pipeline */
+  /* Set up the pipeline */
 
-   /* we set the input filename to the source element */
-    g_object_set (G_OBJECT (source), "location", argv[1], NULL);
+  /* we set the input filename to the source element */
+  g_object_set (G_OBJECT (source), "location", argv[1], NULL);
 
-    /* we add a message handler */
-    bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
-    bus_watch_id = gst_bus_add_watch (bus, bus_call, loop);
-    gst_object_unref (bus);
+  /* we add a message handler */
+  bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+  bus_watch_id = gst_bus_add_watch (bus, bus_call, loop);
+  gst_object_unref (bus);
 
-    /* we add all elements into the pipeline */
-    /* file-source | mp3 decoder | alsa-output */
-    gst_bin_add_many (GST_BIN (pipeline),
-                      source, decoder, sink, NULL);
+  /* we add all elements into the pipeline */
+  /* file-source | mp3 decoder | alsa-output */
+  gst_bin_add_many (GST_BIN (pipeline),
+                    source, decoder, sink, NULL);
 
-    /* we link the elements together */
-    /* file-source -> mp3-decoder -> alsa-output */
-    gst_element_link_many (source, decoder, sink);
-    // g_signal_connect (demuxer, "pad-added", G_CALLBACK (on_pad_added), decoder);
+  /* we link the elements together */
+  /* file-source -> mp3-decoder -> alsa-output */
+  gst_element_link_many (source, decoder, sink);
 
-    /* Set the pipeline to "playing" state*/
-     g_print ("Now playing: %s\n", argv[1]);
-     gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  /* Set the pipeline to "playing" state*/
+  g_print ("Now playing: %s\n", argv[1]);
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
 
-     /* Iterate */
-     g_print ("Running...\n");
-     g_main_loop_run (loop);
+  /* Iterate */
+  g_print ("Running...\n");
+  g_main_loop_run (loop);
 
-     /* Out of the main loop, clean up nicely */
-     g_print ("Returned, stopping playback\n");
-     gst_element_set_state (pipeline, GST_STATE_NULL);
+  /* Out of the main loop, clean up nicely */
+  g_print ("Returned, stopping playback\n");
+  gst_element_set_state (pipeline, GST_STATE_NULL);
 
-     g_print ("Deleting pipeline\n");
-     gst_object_unref (GST_OBJECT (pipeline));
-     g_source_remove (bus_watch_id);
-     g_main_loop_unref (loop);
+  g_print ("Deleting pipeline\n");
+  gst_object_unref (GST_OBJECT (pipeline));
+  g_source_remove (bus_watch_id);
+  g_main_loop_unref (loop);
 
-     return 0;
+  return 0;
 
 
   // Player player;
   // ThriftyGramophoneService service(player);
-  
+
   // service.start();
 
-//  boost::shared_ptr<apache::thrift::transport::TTransport> socket(new apache::thrift::transport::TSocket("localhost", 9090));
-//  boost::shared_ptr<apache::thrift::transport::TTransport> transport(new apache::thrift::transport::TBufferedTransport(socket));
-//  boost::shared_ptr<apache::thrift::protocol::TProtocol> protocol(new apache::thrift::protocol::TBinaryProtocol(transport));
-//  ThriftyGramophone::ThriftyGramophoneClient client(protocol);
+  //  boost::shared_ptr<apache::thrift::transport::TTransport> socket(new apache::thrift::transport::TSocket("localhost", 9090));
+  //  boost::shared_ptr<apache::thrift::transport::TTransport> transport(new apache::thrift::transport::TBufferedTransport(socket));
+  //  boost::shared_ptr<apache::thrift::protocol::TProtocol> protocol(new apache::thrift::protocol::TBinaryProtocol(transport));
+  //  ThriftyGramophone::ThriftyGramophoneClient client(protocol);
 
-//  socket->open();
-//  client.play(argv);
-//  socket->close();
+  //  socket->open();
+  //  client.play(argv);
+  //  socket->close();
 }
--- src/player.hpp
+++ src/player.hpp
@@ -6,38 +6,38 @@
 
 namespace ThriftyGramophone {
 
-  enum State {
-    PLAYING,
-    PAUSED,
-    STOPPED
-  };
+enum State {
+  PLAYING,
+  PAUSED,
+  STOPPED
+};
 
-  class Status {
-  private:
-    State state;
-    std::string URI;
-    int time; // How far into the song we are        
-  };
+class Status {
+private:
+  State state;
+  std::string URI;
+  int time; // How far into the song we are
+};
 
-  class Client {
-    virtual std::string next_song() = 0;
-  };
+class Client {
+  virtual std::string next_song() = 0;
+};
 
-  class Player {
-  protected:
-    Status player_status;
-    DeviceRegistry registry;
+class Player {
+protected:
+  Status player_status;
+  DeviceRegistry registry;
 
-  public:
-    int play(std::string uri);
-    Status status();
+public:
+  int play(std::string uri);
+  Status status();
 
-    /**
+  /**
      * Player takes ownership of passed in client
      */
-    int register_client(std::string id, Client* client);
-    int unregister_client(std::string id);
-  };
+  int register_client(std::string id, Client* client);
+  int unregister_client(std::string id);
+};
 }
 
 #endif
